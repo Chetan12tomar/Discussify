becd3a5491d82852ab165335675a211ff5dc316a
Chetan12tomar <56444159+Chetan12tomar@users.noreply.github.com>
2022-09-02 21:44:20 +0530

    Update db.js

--- college-discuss/backend/db.js
+++ college-discuss/backend/db.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
-const url =
-  "mongodb://chets:chets12@cluster0-shard-00-00.9hwdr.mongodb.net:27017,cluster0-shard-00-01.9hwdr.mongodb.net:27017,cluster0-shard-00-02.9hwdr.mongodb.net:27017/clgdiscuss-mern?ssl=true&replicaSet=atlas-qblq7g-shard-0&authSource=admin&retryWrites=true&w=majority";
+
 
 module.exports.connect = () => {
   mongoose
